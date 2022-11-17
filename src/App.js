827618ebb078ec953b84157c1e70ae8782ba7808
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2022-11-17 21:44:19 +0200

    backend communication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [carwashes, setCarwashes] = useState([]);
+
+    async function getUsers(event) {
+        const response = await fetch('http://localhost:8080/carwash');
+        const responseData = await response.json();
+        // console.log(responseData)
+        const loadedCarwashes = [];
+
+        for (const key in responseData) {
+            loadedCarwashes.push({
+                id: responseData[key].id,
+                address: responseData[key].address,
+                name: responseData[key].name
+            })
+        }
+        setCarwashes(loadedCarwashes);
+        event.preventDefault();
+    }
+
+    const carwashList = carwashes.map((carwash) => {
+        return <p key={carwash.id}>id: {carwash.id}, first name: {carwash.address}, last name: {carwash.name}</p>
+    })
+
+    return (
+        <div>
+            <h1>Car wash web app</h1>
+            <h3>By Atlas</h3>
+            <hr/>
+            <button onClick={getUsers}>Fetch Carwashes</button>
+            <hr/>
+            {carwashList}
+        </div>
+    );
 }
 
 export default App;
