869b08a1bf5d9b1309429efd50393f1101fffc42
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2022-12-20 16:40:48 +0200

    routing fix

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -32,37 +32,37 @@ function NavBar() {
    <>
    <nav className='navbar-home'>
     <div className='navbar-container'>
-        <NavLink to="/home" className="navbar-logo" id={"flex"}>
+        <Link to="/home" className="navbar-logo" id={"flex"}>
           <h2 className="first-word">
             Top
           </h2>
           <h2 className="second-word">
            Wash 
           </h2>
-        </NavLink>
+        </Link>
         {/* <div className="menu-icon" onClick={handleClick}>
           <i className={click ? 'fas fa-times': 'fas fa-bars'}></i>
         </div> */}
         <ul className={click ? 'nav-menu active': 'nav-menu'}>
           <li className='nav-item'>
-            <NavLink to ={'/home'} className='nav-links' onClick={closeMobileMenu}>
+            <Link to ={'/home'} className='nav-links' onClick={closeMobileMenu}>
               Home
-            </NavLink>
+            </Link>
           </li>
           <li className='nav-item'>
-            <NavLink to ={'/list-carwash'} className='nav-links' onClick={closeMobileMenu}>
+            <Link to ={'/list-carwash'} className='nav-links' onClick={closeMobileMenu}>
               List CarWash
-            </NavLink>
+            </Link>
           </li>
           <li className='nav-item'>
-            <NavLink to ={'/register-carwash'} className='nav-links' onClick={closeMobileMenu}>
+            <Link to ={'/register-carwash'} className='nav-links' onClick={closeMobileMenu}>
               Register CarWash
-            </NavLink>
+            </Link>
           </li>
           <li className='nav-item'>
-            <NavLink to ={'/about'} className='nav-links' onClick={closeMobileMenu}>
+            <Link to ={'/about'} className='nav-links' onClick={closeMobileMenu}>
               About Us
-            </NavLink>
+            </Link>
           </li>
           </ul>
           {button && <Button onClick={navigateLogin} buttonStyle='btn--outline'>Login</Button>}
