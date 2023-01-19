01334eb29793282f3c4abf20ac3072e894a07a7b
Farchi <farcas683@gmail.com>
2023-01-20 00:59:45 +0200

    design improvements to home

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
-import './Home.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fafacebook } from '@fortawesome/free-solid-svg-icons'
+
 
 function Home() {
     return (
         <>
                 <div className="home-container">
-                    <h2>Home</h2>
+                    <div className="text-container">
+                        <div className="text-only">
+                        <div className="top-text">
+                                Save your time with
+                        </div>
+                        <div className="bottom-text">
+                                Top Wash
+                        </div>
+                    </div>
+                    <div className="bubbles">
+                    <i className="fa-thin fa-chart-scatter-bubble"></i>
+
+                        </div>
+                    </div>
                 </div>
         </>
     )
