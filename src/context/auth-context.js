847db6c6d6bf6140e4756d835a30dd78790be62a
Romulus Andrei Bacila <30293758+Tolsi111@users.noreply.github.com>
2023-01-20 20:13:45 +0200

    get customer appointments (#5)
    Co-authored-by: AndreiBacila <andrei.bacila@rodeapps.com>

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -4,33 +4,38 @@ const AuthContext = React.createContext({
     isLoggedIn: false,
     email: '',
     role: '',
+    id: '',
     onLogout: () => {},
-    onLogin: (email,role) => {}
+    onLogin: (email,role,id) => {}
 });
 
 
 export const AuthContextProvider = (props) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [email, setEmail] = useState('');
+    const [id, setId] = useState(0);
     const [role, setRole] = useState('');
 
     const logoutHandler = () => {
         setIsLoggedIn(false);
         setEmail('');
         setRole('');
+        setId(0);
         console.log("Logged out!")
     }
 
-    const loginHandler = (theEmail, theRole) => {
+    const loginHandler = (theEmail, theRole, theId) => {
         setIsLoggedIn(true);
         setEmail(theEmail);
         setRole(theRole);
+        setId(theId);
     }
 
     return (<AuthContext.Provider value={{
         isLoggedIn: isLoggedIn, 
         email: email,
         role: role,
+        id: id,
         onLogout: logoutHandler,
         onLogin: loginHandler
         }}>
